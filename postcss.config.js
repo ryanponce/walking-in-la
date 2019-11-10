285256859eee17cfe43e56acb8469aabe5586346
Ryan Ponce <ryan@ponce.la>
2019-11-09 18:21:39 -0800

    Updates postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
   content: [
-    "./src/**/*.tsx",
+    "./pages/**/*.tsx",
+    "./components/**/*.tsx",
   ],
 
   // Include any special characters you're using in this regular expression
