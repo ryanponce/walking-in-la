6ec5d79655eba186460755c552d82eb6564c797a
Ryan Ponce <ryan@ponce.la>
2019-11-09 18:27:09 -0800

    Moves global styles into app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
 
     return (
-      <div className="p-4 max-w-3xl mx-auto">
+      <div className="font-sans bg-green-200 p-4 max-w-3xl mx-auto">
         <Nav />
 
         <main className="my-8">
